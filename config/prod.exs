d2ff4d681fba4c5a81819733b6bc615f0ff93d62
Rohan Pujari <rohanpujaris@gmail.com>
2018-07-15 17:18:50 +0530

    Trail 4

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :real_world, RealWorldWeb.Endpoint,
+  load_from_system_env: true,
   url: [scheme: "https", host: "https://real-world-example.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
