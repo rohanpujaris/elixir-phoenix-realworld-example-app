3d94875d2a9ed461295b549bf186b4acb98ce4bc
Rohan Pujari <rohanpujaris@gmail.com>
2018-07-15 16:59:42 +0530

    HD

--- config/prod.exs
+++ config/prod.exs
@@ -2,8 +2,7 @@ use Mix.Config
 
 config :real_world, RealWorldWeb.Endpoint,
   load_from_system_env: true,
-  http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "https://real-world-example.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "https://real-world-example.herokuapp.com/", port: System.get_env("PORT")],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
